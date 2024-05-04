b563f1b8340c76c2236e645099736249f9abcc8a
Eineleine <dsa210487@mail.ru>
2024-05-04 18:57:28 +0300

    First task solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant < 0) {
+		return arr;
+	} else if (discriminant === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+		return arr;
+	} else {
+		let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr.push(x1, x2);
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (typeof percent !== "number" || typeof contribution !== "number" || typeof amount !== "number" || typeof countMonths !== "number") {
+		return false;
+	}
+	let percentMonth = percent / 100 / 12;
+	let creditBody = amount - contribution;
+	let payment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+	let totalAmount = payment * countMonths;
+	return +totalAmount.toFixed(2);
 }
