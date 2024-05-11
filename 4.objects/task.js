9e45ffe4a378dd51cf470f033d8a9acadb5d1733
Eineleine <dsa210487@mail.ru>
2024-05-11 05:38:06 +0300

    The fourth task is ready

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,40 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+let student1 = new Student("Ivan", "male", 19);
+let student2 = new Student("Mary", "female", 18);
+let student3 = new Student("Ann", "female", 21);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
+    }
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || (this.marks === 0)) {
+        return 0;
+      }
+		let avg = this.marks.reduce((acc, item, index, marks) => {
+			acc += item;
+			if (index === marks.length - 1) {
+				return acc / marks.length;
+			}
+			return acc;
+		}, 0)
+		return avg;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
