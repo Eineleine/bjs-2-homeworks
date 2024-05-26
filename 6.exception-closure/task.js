77e27d86160f7820ca4a40510d4ee5cce3b14c4e
Eineleine <dsa210487@mail.ru>
2024-05-26 05:08:50 +0300

    The sixth task is ready

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(number) {
+	const parsedNumber = Number.parseFloat(number);
+	if (Number.isNaN(parsedNumber)) {
+		throw new Error('Невалидное значение');
+	}
+	return parsedNumber;
+}
+
+function validateCount(number) {
+	try {
+		return parseCount(number);
+	} catch (error) {
+		return error;
+	}
+}
+
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		if (this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
+	get area() {
+		let p = (this.a + this.b + this.c) / 2;
+		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+			get area() {
+				return "Ошибка! Треугольник не существует"
+			},
+			get perimeter() {
+				return "Ошибка! Треугольник не существует"
+			}
+		}
+	}
+}
