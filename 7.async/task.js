a5ac160e17628c75744fd9cc747c834611cc358f
Eineleine <dsa210487@mail.ru>
2024-06-08 04:21:47 +0300

    Seventh task is ready

---
+++ 7.async/task.js
@@ -0,0 +1,72 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		}
+
+		if (this.alarmCollection.some(alarm => alarm.time === time)) {
+			console.warn('Уже присутствует звонок на это же время');
+			return;
+		}
+
+		this.alarmCollection.push({
+			time,
+			callback,
+			canCall: true
+		});
+	}
+
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		let date = new Date();
+		let hours = date.getHours();
+		let minutes = date.getMinutes();
+		if (hours < 10) {
+			hours = '0' + hours;
+		}
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		return hours + ':' + minutes;
+	}
+
+	start() {
+		if (this.intervalId !== null) {
+			return;
+		}
+
+		this.intervalId = setInterval(() => {
+			this.alarmCollection.forEach(item => {
+				if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+					item.canCall = false;
+					item.callback();
+				}
+			});
+		}, 1000);
+	}
+
+	stop() {
+		if (this.intervalId === null) {
+			return;
+		}
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	}
+	resetAllCalls() {
+		this.alarmCollection.forEach((item) => {
+			item.canCall = true
+		});
+	}
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
